b7fa40dd5797a893a7ecfe78df8b816dc520209c
sinisa-terzic <sinisat.ita21@gmail.com>
2023-10-15 08:55:10 +0200

    noting

--- js/code.js
+++ js/code.js
@@ -2,7 +2,7 @@
 // SCROLL TOP / BOTTOM OF THE SECTION
 
 // Function to scroll to the top of a section
-/* function scrollToSectionTop(sectionId) {
+function scrollToSectionTop(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
         section.scrollTop = 0;
@@ -46,56 +46,4 @@ scrollBottomButton.addEventListener('click', function () {
     // scrollToCursorLink();
     navbar.scrollTop = navbar.scrollHeight;
     // cursorLinks[cursorLinks.length - 1].classList.remove('active');
-}); */
-
-
-
-
-///////////////////////////////////////////////
-// Function to remove the "active" class from a button
-function removeActiveClass() {
-    const navLinks = document.querySelectorAll('.link');
-    navLinks.forEach(function (link) {
-        link.classList.remove('active'); // Remove .active class from the link
-    });
-}
-
-// Function to scroll to the top of a section description
-function scrollToSectionTop(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.scrollTop = 0;
-}
-// Function to scroll to the top of a section description
-function scrollToNavbarTop(sectionId) {
-    const navbar = document.getElementById(sectionId);
-    navbar.scrollTop = 0;
-    removeActiveClass();
-}
-/* Event listener for scrolling to the top */
-const scrollTopButton = document.querySelector('.scrollTop');
-scrollTopButton.addEventListener('click', function () {
-    scrollToSectionTop('description');
-    scrollToNavbarTop('navbar');
-});
-
-
-// Function to scroll to the bottom of the section
-function scrollToSectionBottom(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.scrollTop = section.scrollHeight;
-}
-// Function to scroll to the bottom of the section
-function scrollToNavbarBottom(sectionId) {
-    const navbar = document.getElementById(sectionId);
-    // Adjust the timeout for slower scrolling
-    timeoutNavbar = setTimeout(() => {
-        navbar.scrollTop = navbar.scrollHeight;
-    }, 1000); // Increase the timeout for slower scrolling
-    removeActiveClass();
-}
-/* Event listener for scrolling to the bottom */
-const scrollBottomButton = document.querySelector('.scrollBottom');
-scrollBottomButton.addEventListener('click', function () {
-    scrollToSectionBottom('description');
-    scrollToNavbarBottom('navbar');
 });
